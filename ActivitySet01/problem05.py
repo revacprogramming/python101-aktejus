f17343b129f3a7b8edf1fa78446b7da6cafa4aa8
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-05-13 04:01:05 +0000

    P05 done

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,25 @@
 # Functions
 
+score = input("Enter Score: ")
+s =float(score)
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+if s>=1.0:
+    print("Error")
+    
+elif s<=0.0:
+  	print("Error")
+    
+elif s>=0.9:
+	print("A")
+       
+elif s>=0.8:
+    print("B")
+                  
+elif s>=0.7:
+    print("C")
+    
+elif s>=0.6:
+    print("D")
+    
+elif s<0.6:
+    print("F")
