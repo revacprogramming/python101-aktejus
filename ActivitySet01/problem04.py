2bba4c865ff6bb7b57e39e973c92c704c5b42ebc
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-05-13 03:50:55 +0000

    P04 done

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,23 @@
-# Conditional Execution
+score = input("Enter Score: ")
+s =float(score)
 
-hrs = input("Enter hours? ")
+if s>=1.0:
+    print("Error")
+    
+elif s<=0.0:
+  	print("Error")
+    
+elif s>=0.9:
+	print("A")
+       
+elif s>=0.8:
+    print("B")
+                  
+elif s>=0.7:
+    print("C")
+    
+elif s>=0.6:
+    print("D")
+    
+elif s<0.6:
+    print("F")
