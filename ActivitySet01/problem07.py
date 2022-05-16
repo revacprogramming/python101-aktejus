c7d80268836349656bb3e800f4975ca3fff6bb91
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-05-16 16:51:25 +0000

    ..

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,2 +1,25 @@
-# Strings
+# Loops and iteration
+
+largest = 0
+smallest = 0
+print("Invalid output")
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    a= input("A number: ")
+    if a == "done":
+        break
+
+    
+def compare(num,num1,largest,smallest):
+	if a>num:
+         return a
+         
+        
+        
+         
+
+print("Maximum", a)        
+print("Minimum",smallest)
 
--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,17 @@
 # Files
 
 filename = "dataset/mbox-short.txt"
+largest = None
+smallest = None
+print("Invalid input")
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+        
+	for num_i in[num]:
+	    if num > largest:
+	    	largest = num
+   
+
+print("Maximum", largest)
