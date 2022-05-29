87f6d624253d26b7fc2da6f263b8a7f0c289333e
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-05-29 16:55:33 +0000

    P10

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,7 +1,13 @@
 # File
 
-fname= input('Enter the file name')
-try:
-	fh= open(fname)
-except:
-    print("The file entered is invalid")
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+num = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    else:
+        num += float(line[line.find(' '):]) 
+        count += 1
+print('Average spam confidence:', num/count)
