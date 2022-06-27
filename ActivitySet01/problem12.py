6d36d7e8b6ef8dae499ebcd1db03b62a92e8953c
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-06-27 14:20:05 +0000

    problem-12 done

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -2,12 +2,10 @@
 # https://www.py4e.com/lessons/regex
 
 def isPalindrome(s):
-    return s ==s[::-1]
-# Driver code
+  return s ==s[::-1]
 s = "hello"
 ans =isPalindrome(s)
- 
-if ans:
-    print("Yes")
+ if ans:
+  print("Yes")
 else:
-    print("No")
+  print("No")
