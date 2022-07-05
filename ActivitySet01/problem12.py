fa2cab7bcb139022b63efb6759f562b7ed6537b7
Tejus A K <97089330+aktejus@users.noreply.github.com>
2022-07-05 09:38:13 +0000

    Done

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,11 +1,24 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+#Dictionaries 
+fname = input("Enter file:")
+if len(fname) < 1 : name = "mbox-short.txt"
+hand = open(fname)
 
-def isPalindrome(s):
-  return s ==s[::-1]
-s = "hello"
-ans =isPalindrome(s)
- if ans:
-  print("Yes")
-else:
-  print("No")
+lst = list()
+
+for line in hand:
+    if not line.startswith("From:"): continue
+    line = line.split()
+    lst.append(line[1])
+
+counts = dict()
+for word in lst:
+    counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+for word,count in counts.items(): 
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+
+print (bigword,bigcount)
--- ActivitySet01/problem13.py
+++ ActivitySet01/problem13.py
@@ -1,5 +1,21 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
-r=float(input("Enter the radius:"))
-area = 3.14 *r *r
-print('The area of the circle is ',area)
+#Tuples
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "File1.py"
+handle = open(name)
+d=dict()
+for line in handle:
+    if not line.startswith("From "): 
+        continue
+    else:    
+        line=line.split()
+        line=line[5]
+        line=line[0:2]
+        d[line]=d.get(line,0)+1
+
+lst=list()        
+for value,count in d.items():
+    lst.append((value,count))
+
+lst.sort()
+for value,count in lst:
+    print(value,count)
